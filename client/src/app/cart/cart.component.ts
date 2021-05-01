d9196e2698c4db45382cc6f02708d965f287e38f
souhardyasaha98@gmail.com <souhardyasaha98@gmail.com>
2021-05-01 18:50:04 +0600

    Cart Table Added

--- client/src/app/cart/cart.component.ts
+++ client/src/app/cart/cart.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {
+  AddItemAction,
+  LoadCartAction,
+  RemoveItemAction,
+} from '../store/actions/cart.actions';
+import { AppState } from '../store/models/app-state.model';
+import { CartItem } from '../cart/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
+  cartItems$: Observable<CartItem[]>;
+  loading$: Observable<Boolean>;
+  error$: Observable<Error>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.cartItems$ = this.store.select((store) => store.cart.items);
+    this.loading$ = this.store.select((store) => store.cart.loading);
+    this.error$ = this.store.select((store) => store.cart.error);
+
+    this.store.dispatch(new LoadCartAction());
   }
 
+  onRemoveItem(cartItem: CartItem) {
+    this.store.dispatch(new RemoveItemAction(cartItem));
+  }
 }
